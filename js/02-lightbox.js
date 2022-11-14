d4d7c503ed98aa89be06269180c9175625f95472
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-14 15:14:43 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryContainerItems = createGalleryContainerItems(galleryItems);
+
+function createGalleryContainerItems(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<ul class="gallery">
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </ul>`;
+    })
+    .join("");
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryContainerItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+  });
